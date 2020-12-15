9f3c0bac37ba240a6c47ddf31e87c6652cbca76a
Arlen Vasconcelos <arlenvasconcelos@gmail.com>
2020-12-15 20:06:37 -0300

    add  global styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <h1>Shopping Cart</h1>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
