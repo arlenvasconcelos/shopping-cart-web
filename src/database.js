fce40e43c9161a1b4fc6d2edc0421753fe1642f3
Arlen Vasconcelos <arlenvasconcelos@gmail.com>
2020-12-16 23:17:36 -0300

    add database file: mockup data

---
+++ src/database.js
@@ -0,0 +1,57 @@
+export const categories = [
+  {
+    id: 0,
+    name: 'Bebidas',
+  },
+  {
+    id: 1,
+    name: 'Frutas e Verduras',
+  },
+  {
+    id: 2,
+    name: 'Frios',
+  },
+  {
+    id: 3,
+    name: 'Carnes',
+  },
+];
+
+export const products = [
+  {
+    id: 0,
+    name: 'Coca-cola (lata)',
+    description: 'Coca-cola - lata (350ml)',
+    price: 3.50,
+    categoryId: 0,
+  },
+  {
+    id: 1,
+    name: 'Água Mineral',
+    description: 'Água mineral - garrafa (500ml)',
+    price: 2.00,
+    categoryId: 0,
+  },
+  {
+    id: 2,
+    name: 'Suco de Laranja',
+    description: 'Suco de Laranja - Jarra (1 litro)',
+    price: 8.00,
+    categoryId: 0,
+  },
+  {
+    id: 3,
+    name: 'Tomate',
+    description: 'Tomate (1kg)',
+    price: 5.50,
+    categoryId: 1,
+  },
+  {
+    id: 4,
+    name: 'Picanha',
+    description: 'Picanha a vácuo (1kg)',
+    price: 35.00,
+    categoryId: 3,
+  },
+
+];
