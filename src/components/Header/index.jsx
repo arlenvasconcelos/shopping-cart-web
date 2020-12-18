1c5d868c28f79480c25a2bff70f07ddf73925071
Arlen Vasconcelos <arlenvasconcelos@gmail.com>
2020-12-18 01:58:15 -0300

    add quantity of items on header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { FiShoppingCart, FiChevronLeft } from 'react-icons/fi';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { Container, ShoppingCartContent } from './styles';
 
 export default function Header() {
+  const { items } = useSelector((state) => state.shoppingCart);
+
   return (
     <>
       <Container>
@@ -14,7 +17,11 @@ export default function Header() {
         {/* <img src="asas" alt="Logo" /> */}
         <span>Shopping Cart</span>
         <ShoppingCartContent>
-          <span>1</span>
+          {
+            !!items.length && (
+              <span>{items.length}</span>
+            )
+          }
           <Link to="/shopping-cart">
             <FiShoppingCart size="30" />
           </Link>
