e30bc4bf162169ea88725ebabf881a79e06d031f
Arlen Vasconcelos <arlenvasconcelos@gmail.com>
2020-12-18 10:36:44 -0300

    Add modal to cart preview

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,10 +3,12 @@ import { FiShoppingCart, FiChevronLeft } from 'react-icons/fi';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import { Container, ShoppingCartContent } from './styles';
+import formatValue from '../../utils/formatValue';
+
+import { Container, ShoppingCartContent, ShoppingCartModal } from './styles';
 
 export default function Header() {
-  const { items } = useSelector((state) => state.shoppingCart);
+  const { items, total } = useSelector((state) => state.shoppingCart);
 
   return (
     <>
@@ -25,6 +27,24 @@ export default function Header() {
           <Link to="/shopping-cart">
             <FiShoppingCart size="30" />
           </Link>
+
+          <ShoppingCartModal>
+            <div>
+              <FiShoppingCart />
+              <p>Meu carrinho</p>
+            </div>
+            {items.map((item) => (
+              <div>
+                <span>{item.product.name}</span>
+                <span>{formatValue(item.product.price)}</span>
+              </div>
+            ))}
+
+            <div>
+              <p>Total</p>
+              <p>{formatValue(total)}</p>
+            </div>
+          </ShoppingCartModal>
         </ShoppingCartContent>
 
       </Container>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -25,13 +25,13 @@ export const Container = styled.nav`
 
 export const ShoppingCartContent = styled.div`
   position: relative;
-  a{
+  > a{
     background-color: transparent;
     border: 0;
     color: #9466FF;
   }
 
-  span{
+  > span{
     position: absolute;
     top: -3px;
     right: -3px;
@@ -45,4 +45,74 @@ export const ShoppingCartContent = styled.div`
 
   }
 
+  &:hover{
+    > div{
+      display: block;
+
+    }
+  }
+
+`;
+
+export const ShoppingCartModal = styled.div`
+  display: none;
+  background-color: #fff;
+  border-radius: 3px;
+  box-shadow: 1px 1px 10px rgba(0,0,0,0.2);
+  position: absolute;
+  top: 40px;
+  right: 0;
+  width: 250px;
+  font-size: 15px;
+  padding: 8px;
+
+  > div{
+    /* position: relative; */
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    &:first-child{
+      font-weight: bold;
+      font-size: 16px;
+
+      justify-content: start;
+      text-align: center;
+
+      margin-bottom: 16px;
+      padding-bottom: 8px;
+
+      border-bottom: 1px solid #aaa;
+      > svg {
+        margin-right: 6px;
+      }
+
+      > p{
+        flex: 1;
+      }
+    }
+
+    & + div{
+      margin-top: 8px;
+    }
+
+    > span:last-child{
+      font-weight: bold;
+    }
+
+    &::before {
+      content: '';
+      border-style: solid;
+      border-color: #fff transparent;
+      border-width: 0px 6px 6px 6px;
+      top: -6px;
+      position: absolute;
+      right: 1px;
+      transform: translateX(-50%);
+    }
+
+    &:last-child{
+      margin-top: 24px;
+    }
+  }
 `;
